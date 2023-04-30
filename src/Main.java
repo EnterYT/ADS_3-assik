9170785f26d3849cecbc8977b68ca8f6e1004bdc
Klaud <tokesh04@mail.ru>
2023-04-30 21:45:12 +0600

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        MyArrayListStack myArrayListStack = new MyArrayListStack();
-        System.out.println(myArrayListStack.size());
-        System.out.println(myArrayListStack.isEmpty());
-        myArrayListStack.push(1);
-        myArrayListStack.push(2);
-        System.out.println(myArrayListStack.size());
-        System.out.println(myArrayListStack.isEmpty());
-        System.out.println(myArrayListStack.peek());
+//        MyArrayListStack myArrayListStack = new MyArrayListStack();
+//        myArrayListStack.push(1);
+//        myArrayListStack.pop();
+//        System.out.println(myArrayListStack.peek());
+//        System.out.println(myArrayListStack.size());
+//        System.out.println(myArrayListStack.isEmpty());
     }
 }
--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,4 +1,12 @@
-import java.util.EmptyStackException;
-public class MyArrayListQueue extends MyArrayList{
-
-}
+//import java.util.NoSuchElementException;
+//public class MyArrayListQueue extends MyArrayList{
+//    private MyArrayListQueue list;
+//
+//    MyArrayListQueue(MyArrayListQueue list) {
+//        this.list = list;
+//    }
+//
+//    public void dequeue() {
+//        list.add();
+//    }
+//}
