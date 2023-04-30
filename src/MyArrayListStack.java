258f7502a2acdd8d755bc27c5d73cccc063481ea
Klaud <tokesh04@mail.ru>
2023-04-30 13:34:24 +0600

    Updated

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,9 +1,19 @@
-public class MyArrayListStack<T> extends MyArrayList {
-    private MyArrayList<T> list;
+import java.util.EmptyStackException;
+public class MyArrayListStack extends MyArrayList {
+    private MyArrayList list;
 
     public MyArrayListStack() {
         list = new MyArrayList<>();
     }
 
+    public void push(Object element) {
+        list.add(element);
+    }
 
+    public Object pop() {
+        if (list.size() == 0) {
+            throw new EmptyStackException();
+        }
+        return list.remove(list.size() - 1);
+    }
 }
