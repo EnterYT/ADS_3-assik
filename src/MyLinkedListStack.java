6e7f3e42ede86fb674ae5c985ca01e6842d8a2b0
Klaud <tokesh04@mail.ru>
2023-04-30 22:41:35 +0600

    Updated

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -7,10 +7,18 @@ public class MyLinkedListStack<T> extends MyLinkedList {
         list = new MyLinkedList<>();
     }
 
+    /**
+     * push() - method that adds an element to the top of the stack.
+     * @param element
+     */
     public void push(T element) {
         list.add(element, 0);
     }
 
+    /**
+     * pop() - method that removes and returns the top element of the stack.
+     * @return
+     */
     public T pop() {
         if (isEmpty()) {
             throw new EmptyStackException();
@@ -18,6 +26,10 @@ public class MyLinkedListStack<T> extends MyLinkedList {
         return (T) list.remove(0);
     }
 
+    /**
+     * peek() - method that returns the top element of the stack without removing.
+     * @return
+     */
     public T peek() {
         if (isEmpty()) {
             throw new EmptyStackException();
@@ -25,10 +37,18 @@ public class MyLinkedListStack<T> extends MyLinkedList {
         return (T) list.get(0);
     }
 
+    /**
+     * isEmpty() - method that returns true if the stack is empty or false if it is not.
+     * @return
+     */
     public boolean isEmpty() {
         return list.size() == 0;
     }
 
+    /**
+     * size() - method that returns the number of elements in the stack.
+     * @return
+     */
     public int size() {
         return list.size();
     }
