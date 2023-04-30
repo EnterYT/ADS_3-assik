5ae15bfc0215253d730687ac5535da631201d967
Klaud <tokesh04@mail.ru>
2023-04-30 22:52:33 +0600

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,31 @@
 public class Main {
     public static void main(String[] args) {
 //        MyArrayListStack myArrayListStack = new MyArrayListStack();
-//        myArrayListStack.push(1);
+//        myArrayListStack.push();
 //        myArrayListStack.pop();
-//        System.out.println(myArrayListStack.peek());
-//        System.out.println(myArrayListStack.size());
-//        System.out.println(myArrayListStack.isEmpty());
+//        myArrayListStack.peek();
+//        myArrayListStack.size();
+//        myArrayListStack.isEmpty();
 
+//        MyArrayListQueue myArrayListQueue = new MyArrayListQueue();
+//        myArrayListQueue.enqueue();
+//        myArrayListQueue.dequeue();
+//        myArrayListQueue.peek();
+//        myArrayListQueue.size();
+//        myArrayListQueue.isEmpty();
 
+//        MyLinkedListStack myLinkedListStack = new MyLinkedListStack();
+//        myLinkedListStack.push();
+//        myLinkedListStack.pop();
+//        myLinkedListStack.peek();
+//        myLinkedListStack.size();
+//        myLinkedListStack.isEmpty();
+
+//        MyLinkedListQueue myLinkedListQueue = new MyLinkedListQueue();
+//        myLinkedListQueue.enqueue();
+//        myLinkedListQueue.dequeue();
+//        myLinkedListQueue.peek();
+//        myLinkedListQueue.size();
+//        myLinkedListQueue.isEmpty();
     }
 }
