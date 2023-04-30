e31fe65b78b80a54b235f387ab2944b82a1814b7
Klaud <tokesh04@mail.ru>
2023-04-30 22:14:35 +0600

    Updated

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,2 +1,13 @@
-public class MyLinkedListStack extends MyLinkedList {
+import java.util.EmptyStackException;
+
+public class MyLinkedListStack<T> extends MyLinkedList {
+    private MyLinkedList<T> list;
+
+    public MyLinkedListStack() {
+        list = new MyLinkedList<>();
+    }
+
+    public int size() {
+        return list.size();
+    }
 }
